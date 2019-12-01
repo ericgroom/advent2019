e9377b5c688c55c752e9f072ad1fc9433ce983f9
Eric Groom <egroom@mail.sfsu.edu>
2019-12-01 09:27:12 -0800

    part 2

--- src/day1.rs
+++ src/day1.rs
@@ -5,6 +5,21 @@ fn total_fuel_required(modules: Vec<i32>) -> i64 {
     modules.iter().map(|mass| { fuel_required(*mass) as i64 }).sum()
 }
 
+fn total_fuel_required_with_fuel_cost(modules: Vec<i32>) -> i64 {
+    modules.iter()
+        .map(|mass| { fuel_required(*mass) as i64 })
+        .map(|fuel| { fuel + calculate_fuel_cost(fuel) })
+        .sum()
+}
+
+pub fn calculate_fuel_cost(fuel: i64) -> i64 {
+    let fuel_needed_for_fuel = fuel_required(fuel as i32) as i64;
+    if fuel_needed_for_fuel <= 0 {
+        return 0;
+    }
+    fuel_needed_for_fuel + calculate_fuel_cost(fuel_needed_for_fuel)
+}
+
 pub fn fuel_required(mass: i32) -> i32 {
     mass / 3 - 2
 }
@@ -17,6 +32,11 @@ fn get_test_input() -> Result<Vec<i32>> {
 pub fn get_test_result() -> Result<i64> {
     let modules = get_test_input()?;
     Ok(total_fuel_required(modules))
+}
+
+pub fn get_test_result_with_fuel_cost() -> Result<i64> {
+    let modules = get_test_input()?;
+    Ok(total_fuel_required_with_fuel_cost(modules))
 }
 
 #[cfg(test)]
@@ -38,5 +58,29 @@ mod tests {
         assert_eq!(fuel_required(100756), 33583);
     }
 
+    #[test]
+    fn test_calculate_fuel_cost() {
+        assert_eq!(calculate_fuel_cost(2), 0);
+        assert_eq!(calculate_fuel_cost(654), 966-654);
+        assert_eq!(calculate_fuel_cost(33583), 50346-33583);
+    }
 
+    #[test]
+    fn test_total_with_fuel_cost() {
+        assert_eq!(total_fuel_required_with_fuel_cost(vec!(14)), 2);
+        assert_eq!(total_fuel_required_with_fuel_cost(vec!(1969)), 966);
+        assert_eq!(total_fuel_required_with_fuel_cost(vec!(100756)), 50346);
+    }
+
+    #[test]
+    fn test_correct_answer_part_1() -> Result<()> {
+        assert_eq!(get_test_result()?, 3223398);
+        Ok(())
+    }
+
+    #[test]
+    fn test_correct_answer_part_2() -> Result<()> {
+        assert_eq!(get_test_result_with_fuel_cost()?, 4832253);
+        Ok(())
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,6 @@ use anyhow::Result;
 
 fn main() -> Result<()> {
     println!("Day 1 fuel required: {}", day1::get_test_result()?);
+    println!("Day 1 fuel required with fuel cost: {}", day1::get_test_result_with_fuel_cost()?);
     Ok(())
 }
