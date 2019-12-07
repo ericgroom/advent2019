add2a60549185cc192bde7a4020cef8d1e887ff3
Eric Groom <egroom@mail.sfsu.edu>
2019-12-06 19:35:19 -0800

    working graph construction

--- src/day6.rs
+++ src/day6.rs
@@ -1,6 +1,5 @@
 use std::cell::RefCell;
 use std::collections::HashMap;
-use std::rc::{Rc, Weak};
 
 #[derive(Debug)]
 struct CelestialNode {
@@ -8,34 +7,47 @@ struct CelestialNode {
     orbits: Option<String>,
 }
 
-fn construct_graph(orbit_pairs: Vec<(String, String)>) -> HashMap<String, RefCell<CelestialNode>> {
+fn construct_graph(orbit_pairs: Vec<(&str, &str)>) -> HashMap<String, RefCell<CelestialNode>> {
     let mut vertex_set: HashMap<String, RefCell<CelestialNode>> = HashMap::new();
     for (orbitee_id, orbiter_id) in orbit_pairs {
-        let orbitee_id_clone = orbitee_id.clone();
-        let orbiter_id_clone = orbiter_id.clone();
-        let orbitee = match vertex_set.remove(&orbitee_id) {
+        let orbitee = match vertex_set.remove(&String::from(orbitee_id)) {
             Some(orbitee) => orbitee,
             None => RefCell::new(CelestialNode {
-                id: orbitee_id,
+                id: String::from(orbitee_id),
                 orbits: None,
             }),
         };
-        let orbiter = match vertex_set.remove(&orbiter_id) {
+        let orbiter = match vertex_set.remove(&String::from(orbiter_id)) {
             Some(orbiter) => {
-                if let Some(orbiter_orbits_id) = &orbiter.borrow().orbits {
+                let mut orbiter_ref = orbiter.borrow_mut();
+                if let Some(orbiter_orbits_id) = &orbiter_ref.orbits {
                     panic!("orbiter already orbits: {}", orbiter_orbits_id);
                 } else {
-                    orbiter.borrow_mut().orbits = Some(orbitee.borrow().id.clone());
+                    orbiter_ref.orbits = Some(orbitee.borrow().id.clone());
                 }
+                drop(orbiter_ref);
                 orbiter
             }
             None => RefCell::new(CelestialNode {
-                id: orbiter_id,
+                id: String::from(orbiter_id),
                 orbits: Some(orbitee.borrow().id.clone()), // set below to avoid move
             }),
         };
-        vertex_set.insert(orbitee_id_clone, orbitee);
-        vertex_set.insert(orbiter_id_clone, orbiter);
+        vertex_set.insert(String::from(orbitee_id), orbitee);
+        vertex_set.insert(String::from(orbiter_id), orbiter);
     }
     vertex_set
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_construct_graph() {
+        let pairs = vec![("A", "B"), ("B", "C"), ("COM", "A")];
+        let vertex_graph = construct_graph(pairs);
+        assert_eq!(vertex_graph["C"].borrow().orbits.as_ref().unwrap(), "B");
+        assert_eq!(vertex_graph["B"].borrow().orbits.as_ref().unwrap(), "A");
+        assert_eq!(vertex_graph["A"].borrow().orbits.as_ref().unwrap(), "COM");
+    }
 }
